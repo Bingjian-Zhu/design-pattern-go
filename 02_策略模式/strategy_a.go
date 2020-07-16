d2c9a21cd20c15e79117bbb26720b7c9dfa8ee54
root <root@DESKTOP-NGTHS2N.localdomain>
2020-07-16 23:43:48 +0800

    策略模式

--- 02_策略模式/strategy_a.go
+++ 02_策略模式/strategy_a.go
@@ -13,3 +13,41 @@ type CashNormal struct {
 func (*CashNormal) AcceptCash(money float64) float64 {
 	return money
 }
+
+//CashRebate 打折收费
+type CashRebate struct {
+	moneyRebate float64
+}
+
+//NewCashRebate 构造函数，返回CashRebate
+func NewCashRebate(moneyRebate float64) CashSuper {
+	return &CashRebate{
+		moneyRebate: moneyRebate,
+	}
+}
+
+//AcceptCash 打折收费，乘以折扣
+func (rebate *CashRebate) AcceptCash(money float64) float64 {
+	return money * rebate.moneyRebate
+}
+
+//CashReturn 返利收费
+type CashReturn struct {
+	moneyCondition, moneyReturn float64
+}
+
+//NewCashReturn 构造函数，返回CashReturn
+func NewCashReturn(moneyCondition, moneyReturn float64) CashSuper {
+	return &CashReturn{
+		moneyCondition: moneyCondition,
+		moneyReturn:    moneyReturn,
+	}
+}
+
+//AcceptCash 返利收费
+func (re *CashReturn) AcceptCash(money float64) float64 {
+	if money >= re.moneyCondition {
+		money -= re.moneyReturn
+	}
+	return money
+}
